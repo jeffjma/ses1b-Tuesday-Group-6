ffc633b7f173e59490c7fc5e8e962904775af58b
Jeffrey <13196530@student.uts.edu.au>
2020-08-30 20:11:32 +1000

    Login Screen
    Forgot to commit all changes

--- ERest/app/src/main/java/com/example/erest/MainActivity.java
+++ ERest/app/src/main/java/com/example/erest/MainActivity.java
@@ -8,23 +8,37 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     private Button mBtnLogin;
-    private EditText mEtemail;
-    private EditText mEtpassword;
+    private Button mBtnReg;
+    private TextView mTvTitle;
+    private EditText mEtEmail;
+    private EditText mEtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.view_main);
         //button 声明
         //1. button order
-        mBtnLogin = findViewById(R.id.btn_loginid);
+        mBtnLogin = findViewById(R.id.btn_login);
+        mBtnReg = findViewById(R.id.btn_reg);
+        mTvTitle = findViewById(R.id.tv_header);
+        mEtEmail = findViewById(R.id.et_email);
+        mEtPassword = findViewById(R.id.et_password);
+
+        //Login Button when clicked run validation method
         mBtnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // to menu界面
-                Intent intent = new Intent(MainActivity.this,MenuActivity.class);
-                startActivity(intent);
+                validate(mEtEmail.getText().toString(), mEtPassword.getText().toString());
             }
         });
     }
+    //validation method
+    private void validate (String userEmail, String userPassword) {
+        if((userEmail.equals("test@gmail.com")) && (userPassword.equals("password"))) {
+            Intent intent = new Intent(MainActivity.this,MenuActivity.class);
+            startActivity(intent);
+        }
+    }
+
 }
